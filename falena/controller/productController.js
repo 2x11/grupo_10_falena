ac783979a02c313d3863c169f416be3690558626
sebastianabila <sebastianabila2015@gmail.com>
2020-09-09 11:20:45 -0300

    cree la vista index

--- falena/controller/productController.js
+++ falena/controller/productController.js
@@ -1,4 +1,20 @@
+/*
+* IMPORTAR LIBRERIA Y ARCHIVOS
+*/
+const path = require('path');
+const dbProducts = require(path.join(__dirname,'..','data','dbProducts'));
+const dbCategory = require(path.join(__dirname,'..','data','dbCategory'));
+
+
 module.exports = {
+    index: function(req, res) {
+        res.render('product', { 
+            title: 'Express', 
+            css: 'index.css', 
+            menu: 'admin',
+            products: dbProducts 
+        });
+    },    
     detail: function(req, res){
         res.render('productDetail',{
             css: 'product.css',
--- falena/routes/product.js
+++ falena/routes/product.js
@@ -9,16 +9,12 @@ let productController = require('../controller/productController')
 /*
 * Routes
 */
+router.get('/', productController.index);
+
 router.get('/detail/1', productController.detail);
 
 router.get('/add', productController.add);
 
-router.get('/', function(req, res) {
-    res.render('product', { 
-        title: 'Express', 
-        css: 'index.css', 
-        menu: 'admin' 
-    });
-  });
+
 
 module.exports = router;
