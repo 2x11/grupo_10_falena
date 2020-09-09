7b0d57d2604e56784cd50370e0a4f59e24bbc2c4
sebastianabila <sebastianabila2015@gmail.com>
2020-09-09 11:15:48 -0300

    agrege method override

--- falena/app.js
+++ falena/app.js
@@ -3,7 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+/*
+* AGREGADAS 
+*/
+const methodOverride = require('method-override');
 /*
 * IMPORTAR ARCHIVOS
 */
@@ -17,7 +20,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+/*
+* AGREGADAS
+*/
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
