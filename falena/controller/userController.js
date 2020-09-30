2dde372e4f065bcbcd14679e28db740eea3ec4f9
SantiagoUria <67654994+SantiagoUria@users.noreply.github.com>
2020-09-30 19:15:18 -0300

    agrega funcionalidad a register

--- falena/controller/userController.js
+++ falena/controller/userController.js
@@ -1,15 +1,53 @@
-let userController = {
+const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+
+// const {validationResult} = require('express-validator');
+
+
+const dbUser = require(path.join(__dirname,'..','data','dbUser'))
+
+
+module.exports = {
     login: (req, res, next)=>{
         res.render('login',{
             css: 'login.css',
             menu: 'user'
         });
     },
-    register: (req, res, next)=>{
+    loginProcess: (req, res, next)=>{
+
+        
+    },
+    register: (req, res)=>{
         res.render('register',{
             css: 'register.css',
             menu: 'user'
         });
+    },
+    registerProcess:(req,res)=>{
+        let lastID = 0;
+        if(dbUser.length != 0){
+            dbUser.forEach(user => {
+                if(user.id > lastID){
+                    lastID = user.id
+                }
+            });
+        };
+            let newUser = {
+                id: lastID + 1,
+                first_name: req.body.firstname.trim(),
+                last_name: req.body.lastname.trim(),
+                dni: req.body.dni.trim(),
+                email: req.body.email.trim(),
+                password:bcrypt.hashSync(req.body.password,10),
+                rol:"user"
+            }
+            dbUser.push(newUser);
+            fs.writeFileSync(path.join(__dirname,'..','data','dbUser.json'),JSON.stringify(dbUser),'utf-8')
+    
+            return res.redirect('/user/login');
+       
     },
     cart: (req, res, next)=>{
         res.render('cart',{
@@ -18,4 +56,3 @@ let userController = {
         })
     }
 }
-module.exports = userController;
