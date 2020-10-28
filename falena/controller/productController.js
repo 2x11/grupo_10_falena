3ccc7e21e9842e8e5a7702c7933be2fed6acab86
sebastianabila <sebastianabila2015@gmail.com>
2020-10-28 11:04:46 -0300

    crud terminado de producto

--- falena/controller/productController.js
+++ falena/controller/productController.js
@@ -1,114 +1,121 @@
 /*
  * IMPORTAR LIBRERIA Y ARCHIVOS
  */
-const path = require('path');
-const fs = require('fs');
-const dbProducts = require(path.join(__dirname, '..', 'data', 'dbProducts'),'utf-8');
-const dbCategory = require(path.join(__dirname, '..', 'data', 'dbCategory'),'utf-8');
+const { reset } = require('nodemon');
+const Sequelize = require('sequelize');
+const db = require('../database/models');
+let Op = Sequelize.Op;
 
 
 module.exports = {
     index: function(req, res) {
-        res.render('product', {
-            title: 'Express',
-            css: 'index.css',
-            menu: 'admin',
-            products: dbProducts
-        });
-    },
-    /*
-    listar:function(req,res){
-        res.render('product',{
-            title: 'productos',
-            css:'index.css',
-            menu: 'user',
-            productos:dbProducts
-
+        db.Products.findAll()
+        .then(Products => {
+            res.render('product', {
+                title: 'Express',
+                css: 'index.css',
+                menu: 'admin',
+                products: Products
+            });            
         })
     },
-   */
     detail: function(req, res) {
-        idProducto = req.params.id;
-        let product = dbProducts.filter(producto => {
-            return producto.id == idProducto
+        db.Products.findOne({
+            where : {
+                id : req.params.id
+            },
+            include : [
+                {
+                    association : 'Categories'
+                }
+            ]            
+        }).then( Products => {
+            res.render('productDetail', {
+                css: 'product.css',
+                menu: 'user',
+                product: Products,
+                category : Products.Categories
+            });
         })
-        res.render('productDetail', {
-            css: 'product.css',
-            menu: 'user',
-            product: product[0]
-        });
     },
     add: function(req, res) {
-        res.render('productAdd', {
-            css: 'product.css',
-            menu: 'admin',
-            category: dbCategory
-        });
+        db.Categories.findAll({
+            order : [
+                ['name','ASC']
+            ]
+        })
+        .then(categories => {
+            res.render('productAdd', {
+                css: 'product.css',
+                menu: 'admin',
+                category: categories
+            });            
+        })
     },
     save: function(req, res, next) {
-        let lastID = 1;
-        dbProducts.forEach(producto => {
-            if (producto.id > lastID) {
-                lastID = producto.id
-            }
-        })
-        let newProduct = {
-            id: lastID + 1,
-            author: req.body.author,
+         db.Products.create({
             name: req.body.name,
-            price: Number(req.body.price),
-            discount: Number(req.body.discount),
+            author: req.body.author,
+            price: req.body.price,
+            discount: req.body.discount,
             review: req.body.review,
-            category: req.body.category,
+            category_id: req.body.category,
             section: req.body.section,
-            image: req.files[0].filename
-        }
-        dbProducts.push(newProduct);
-        fs.writeFileSync(path.join(__dirname, "..", "data", "product.json"), JSON.stringify(dbProducts), 'utf-8')
-        res.redirect('/product')
+            image: req.files[0].filename,
+            rating: req.body.rating
+        }).then(newProducts => {
+            res.redirect('/product')            
+        })    
     },
 
     editForm: (req, res) => {
-        let prodToEdit;
-        dbProducts.forEach(product => {
-            if(product.id == req.params.id){
-                prodToEdit = product;
-            }          
-        });
-        res.render('productEdit', {
-            css: 'product.css',
-            menu: 'admin',
-            prodToEdit:prodToEdit,
-            category: dbCategory
-        });
+        let pedidoProducts = db.Products.findByPk(req.params.id)
+        let pedidosCategories = db.Categories.findAll();
+
+        Promise.all([pedidoProducts,pedidosCategories ])
+        .then(([products, categories]) => {
+            res.render('productEdit', {
+                css: 'product.css',
+                menu: 'admin',
+                products:products,
+                category: categories
+            });            
+        })
+
     },
 
     edit: (req,res)=>{
-        dbProducts.forEach(product =>{
-            if (product.id == req.params.id) {
-                product.name = req.body.name;
-                product.price = req.body.price;
-                product.category = req.body.category;
-                product.review = req.body.review;                
-            };
-        });
-
-        fs.writeFileSync(path.join(__dirname, '..', 'data', 'product.json'), JSON.stringify(dbProducts),'utf-8');
-
-        res.redirect('/product/detail/' + req.params.id);
+        db.Products.update({
+            name: req.body.name,
+            author: req.body.author,
+            price: req.body.price,
+            discount: req.body.discount,
+            review: req.body.review,
+            category_id: req.body.category,
+            section: req.body.section,
+            rating: req.body.rating        
+        },
+        {
+            where: {
+            id: req.params.id
+            }            
+        })
+        .then(resultado => {
+            res.redirect('/product');
+        }).catch(err => {
+            console.log(err)
+        }) 
+        
     },
 
     delete: (req, res) => {
-        let idProducto;
-        dbProducts.forEach(producto => {
-            if (producto.id == req.params.id) {
-                idProducto = dbProducts.indexOf(producto)
+        db.Products.destroy({
+            where: {
+                id: req.params.id
             }
-        });
-        dbProducts.splice(idProducto, 1);
-
-
-        fs.writeFileSync(path.join(__dirname, "..", "data", "product.json"), JSON.stringify(dbProducts), 'utf-8')
-        res.redirect('/product')
-    }
+        })
+        .then(result=>{
+            res.redirect('/product')
+        })
+     }
 }
