90986bfe8b76c545e67606d8720cdb1baa646cb1
SantiagoUria <67654994+SantiagoUria@users.noreply.github.com>
2020-11-15 02:02:24 -0300

    preparaciones para vista y funcionamiento de carrito

--- falena/routes/cart.js
+++ falena/routes/cart.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const cartController = require('../controller/cartController')
-const validaruser = require('../middlewares/validarVisitante')
+const validarUser = require('../middlewares/validarVisitante')
 let locasRol = require('../middlewares/locasRol');
 
 
-router.get('/',  locasRol, validaruser , cartController.cart);
+router.get('/', locasRol, validarUser, cartController.cart);
 
 
 module.exports = router;
