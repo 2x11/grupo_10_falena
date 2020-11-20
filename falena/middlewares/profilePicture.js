a9dcecf30b5dd07075ba4b44efdab85348049d69
SantiagoUria <67654994+SantiagoUria@users.noreply.github.com>
2020-11-20 00:21:41 -0300

    acepta archivos webp

--- falena/middlewares/profilePicture.js
+++ falena/middlewares/profilePicture.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
   }
 });
 const fileFilter = function(req, file,callback) {
-  if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+  if(!file.originalname.match(/\.(jpg|jpeg|png|gif|webp)$/)){
       req.fileValidationError = "Solo Images";
       return callback(null,false,req.fileValidationError);
   }
