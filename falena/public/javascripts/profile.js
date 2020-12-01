dbb74a602ea4fb0170405632a80e4391d1ec4f88
SantiagoUria <67654994+SantiagoUria@users.noreply.github.com>
2020-12-01 10:19:12 -0300

    agrega fetch con api de datos geograficos para los campos de ciudad y localidad

--- falena/public/javascripts/profile.js
+++ falena/public/javascripts/profile.js
@@ -1,21 +1,31 @@
 window.addEventListener('load', () => {
+    
+    let editUserForm = document.querySelector('#profileUpdate-form')
 
     let nickInput = document.querySelector('#nick')
     let firstNameInput = document.querySelector('#first_name')
     let lastNameInput = document.querySelector('#last_name')
     let dniInput = document.querySelector('#dni')
     let phoneNumberInput = document.querySelector('#phone_number')
-    let adressInput = document.querySelector('#adress')
+    let citySelect = document.querySelector('#citySelect')
+    let townSelect = document.querySelector('#townSelect')
+    let cityInput = document.querySelector('#cityInput')
+    let townInput = document.querySelector('#townInput')
     let zipCodeInput = document.querySelector('#zip_code')
     let emailInput = document.querySelector('#email')
 
-    let editUserForm = document.querySelector('#profileUpdate-form')
+    let urlZipCode = 'https://github.com/jcodagnone/localidades-ar/tree/master/codigo-postal/correo'
 
     let saveBtn = document.querySelector('#profile-save')
     let deleteBtn = document.querySelector('#profile-delete')
     let formUpdate = document.querySelector('#profileUpdate-form')
     let formDelete = document.querySelector('#profileDelete-form')
 
+    function ordenarAsc(p_array_json, p_key) {
+        p_array_json.sort(function (a, b) {
+           return a[p_key] > b[p_key];
+        });
+     }
 
     nickInput.addEventListener('blur', () => {
 
@@ -124,6 +134,77 @@ window.addEventListener('load', () => {
                 })
             };
         })
+    })
+
+
+
+
+
+     let townSelectF = function(){
+
+        cityInput.value = citySelect.options[citySelect.selectedIndex].text;
+
+        townSelect.innerHTML = ""
+
+        fetch('https://apis.datos.gob.ar/georef/api/localidades?max=1000&provincia=' + cityInput.value)
+
+        .then(response => response.json())
+
+        .then( result => {
+            
+            ordenarAsc(result.localidades,'nombre');
+
+            result.localidades.forEach(localidad => {
+                townSelect.innerHTML += `<option value=${localidad.id}>${localidad.nombre}</option>`
+            })
+
+            townSelect.style.display = "block";
+            townInput.style.display = "none";
+        })
+     }
+
+
+
+
+    fetch('https://apis.datos.gob.ar/georef/api/provincias')
+  
+    .then(response => response.json())
+
+    .then(result => {
+
+        ordenarAsc(result.provincias,'nombre')
+        
+        result.provincias.forEach(provincia => {
+            citySelect.innerHTML += `<option value=${provincia.id}>${provincia.nombre}</option>`
+        })
+    })
+
+    cityInput.addEventListener('focus',()=>{
+        cityInput.style.display = "none";
+        citySelect.style.display = "block"
+    })
+
+    citySelect.addEventListener('change',()=>{
+        citySelect.style.display = "none";
+        cityInput.style.display = "block";
+
+        
+
+        townSelectF();
+    })
+
+    townSelect.addEventListener('change',() =>{
+        townSelect.style.display = "none";
+        townInput.style.display = "block";
+
+        townInput.value = townSelect.options[townSelect.selectedIndex].text;
+
+    })
+
+    townInput.addEventListener('focus',()=>{
+        townInput.style.display = "none";
+        townSelect.style.display = "block";
+        townSelectF()
     })
 
 })
